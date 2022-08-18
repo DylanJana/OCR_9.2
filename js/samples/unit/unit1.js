14e64274498d806b6d81f9243ec190c6ad5076d5
Dylan Durand <dylan.durand88@outlook.fr>
2022-08-18 16:34:19 +0200

    Partie-1/chapite-2-a
    Découverte du test coverage et mise en application

--- js/samples/unit/unit1.js
+++ js/samples/unit/unit1.js
@@ -7,5 +7,17 @@ export const sayHello = name => {
         return "Hello, World"
     }
 
+    if(name === 'Alexandra') {
+        return "Bonjour Alexandra"
+    }
+
+    if(name === 'McFly') {
+        return "Salut poto McFly"
+    }
+
+    if(name === 'Carlito') {
+        return "Yo frére de McFly"
+    }
+
     return `Hello, ${name}`
 }
